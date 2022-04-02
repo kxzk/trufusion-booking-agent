7f5951772ca95d6ed87c0ddc116fa849354c7cba
kadekillary <kadekillary@pm.me>
2022-04-02 05:31:12 -0700

    Add: class prefs + automated class link generation
    Why:
    
    * need way to communicate which class I want to book
    * don't want to have to manually create link
    
    This change addresses the need by:
    
    * holding my prefs in classPreference
    * from my preference generating a class link

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -10,38 +9,43 @@ import (
 	"strings"
 )
 
-// TODO: reduce all the duplicative code
-// TODO: maybe add .yaml file and read in for configuration
-// TODO: better strucutre/organization
-// TODO: better way to structure all http.NewRequest's
-// TODO: better extraction of utf8 and auth_token
-// TODO: add tests
-
 // TODO: fix this, hacky AF
 const (
 	USERNAME = ""
 	PASSWORD = ""
 )
 
-func main() {
+var (
+	session  = "https://cart.mindbodyonline.com/sites/14486/session/new"
+	login    = "https://cart.mindbodyonline.com/sites/14486/session/"
+	checkout = "https://cart.mindbodyonline.com/sites/14486/cart/proceed_to_checkout"
+)
 
+var classPreference = map[string]int{
+	"Monday":    1, // bootcamp
+	"Tuesday":   1, // bootcamp
+	"Wednesday": 0, // bootcamp
+	"Friday":    0, // kettlebell
+}
+
+func main() {
 	jar, _ := cookiejar.New(nil)
 	c := &http.Client{Jar: jar}
 
-	// TODO: extract out core domain name, more efficient with creating links
-	req, _ := http.NewRequest("GET", "https://cart.mindbodyonline.com/sites/14486/session/new", nil)
+	// TODO: create function for all these requests + responses
+	req, _ := http.NewRequest("GET", session, nil)
 	req.Header.Add("Connection", "keep-alive")
 
 	resp, _ := c.Do(req)
 	defer resp.Body.Close()
 
-	log.Println("Initial Response -> ", resp.StatusCode)
-
 	body, _ := ioutil.ReadAll(resp.Body)
 	strBody := string(body)
 
-	// TODO: improve this match to be more specific, reduce amount of work
-	// I have to do afterwards
+	// TODO: extract out into function
+	// TODO: improve regex to be more exact
+	//
+	// only need `utf8` and `authenticity_token`
 	re := regexp.MustCompile(`<input.*>`)
 
 	matches := re.FindAllStringSubmatch(strBody, -1)
@@ -49,6 +53,8 @@ func main() {
 	splitMatches := strings.Split(matches[0][0], " ")
 
 	var parts []string
+	// parts[0] -> utf8
+	// parts[1] -> authenticity_token
 
 	for _, s := range splitMatches {
 		if strings.HasPrefix(s, "value=") {
@@ -58,47 +64,43 @@ func main() {
 		}
 	}
 
-	log.Println("Parts -> ", parts)
-
-	// parts[0] -> utf8
-	// parts[1] -> authenticity_token
-
+	// TODO: extract out into function
 	user := url.Values{}
 	user.Add("mb_client_session[username]", USERNAME)
 	user.Add("mb_client_session[password]", PASSWORD)
 	user.Add("utf8", parts[0])
 	user.Add("authenticity_token", parts[1])
 
-	log.Println("User -> ", user)
-
-	signIn := "https://cart.mindbodyonline.com/sites/14486/session/"
-
-	req2, _ := http.NewRequest("POST", signIn, strings.NewReader(user.Encode()))
+	// login to mindbodyonline with account
+	req2, _ := http.NewRequest("POST", login, strings.NewReader(user.Encode()))
 	req2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	resp2, _ := c.Do(req2)
 	defer resp2.Body.Close()
 
-	log.Println("SignIn Response -> ", resp2.StatusCode)
+	nextWeekDate := getNextWeekDate()
+	_, isoWeek := nextWeekDate.ISOWeek()
+	dayOfWeek := nextWeekDate.Weekday().String()
 
-	// TODO: figure out how to programatically generate these for each of Katrina's classes
-	// theoretically, will  need to run Mon-Thurs and it will just be current_date() + 7 days
-	// teacher  name and class will be the same
-	//
-	// need to figure out if ID is increasing, random or able to be pre-populated or going
-	// to have to scrape to get it -> item_mbo_id
-	//
-	// Have to have item_mbo_id in URL -> how to get?
-	req3, _ := http.NewRequest("GET", "https://cart.mindbodyonline.com/sites/14486/cart/add_booking?item%5Binfo%5D=Mon.+Apr++4%2C+2022++9%3A00+pm&item%5Bmbo_id%5D=114627&item%5Bmbo_location_id%5D=1&item%5Bname%5D=60+Min.+Hot+Power+Vinyasa+%28Level+1%2F2%29+&item%5Btype%5D=Class&source=schedule_v1&ga_client_id=undefined", nil)
+	// TODO: add check to make sure only runs on Mon-Wed +  Fri
+
+	class := classSchedule[dayOfWeek][classPreference[dayOfWeek]]
+
+	classDate := getFormattedDate(nextWeekDate, class.time)
+	classID := class.id + getClassIDOffset(isoWeek)
+	classURL := classURLFormat[class.name]
+
+	classLink := getClassLink(classDate, classID, classURL)
+
+	// adds desired class to our cart
+	req3, _ := http.NewRequest("GET", classLink, nil)
 
 	resp3, _ := c.Do(req3)
 	defer resp3.Body.Close()
 
-	req4, _ := http.NewRequest("GET", "https://cart.mindbodyonline.com/sites/14486/cart/proceed_to_checkout", nil)
+	// this request will proceed with checking out the class in our cart
+	req4, _ := http.NewRequest("GET", checkout, nil)
 
 	resp4, _ := c.Do(req4)
 	defer resp4.Body.Close()
-
-	log.Println("Checkout Resposne -> ", resp4.StatusCode)
-
 }
